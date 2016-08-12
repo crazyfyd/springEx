c67f8ee71cd29bc1d1ac93bb57e612c6dce68eb7
CRZYDRG <pcr3831@atsolutions.co.kr>
2016-08-12 17:41:09 +0900

    chap02

---
+++ spring_demo/src/main/java/com/exemple/demo/Main.java
@@ -0,0 +1,19 @@
+package com.exemple.demo;
+
+import org.springframework.context.support.GenericXmlApplicationContext;
+
+public class Main {
+
+	public static void main(String[] args) {
+		GenericXmlApplicationContext ctx = new GenericXmlApplicationContext("classpath:applicationContext.xml");
+		chap02Greeter g = ctx.getBean("greeter", chap02Greeter.class);
+		String msg = g.greet("SSSPPPPRRRIIINGG");
+		System.out.println(msg);
+		
+		chap02Greeter g1 = ctx.getBean("greeter", chap02Greeter.class);
+		chap02Greeter g2 = ctx.getBean("greeter", chap02Greeter.class);
+		System.out.println("(g1 == g2) = " + (g1 == g2)); 	// ΩÃ±€≈Ê
+		
+		ctx.close();
+	}
+}
